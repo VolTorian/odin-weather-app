04e81f06cdbdfd7ca1a8911713e5d5d553caa1c6
VolTorian <33360939+VolTorian@users.noreply.github.com>
2024-07-17 14:41:51 -0700

    function to request weather data through console

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const api = "CRPB2EX2LBDSGYUZNHK3LM27N";
+
 console.log(document.querySelector("h1").textContent);
+
+async function getWeather() {
+    let city = prompt("Enter the name of a city:");
+    city = city.replace(/ /g, "%20");
+
+    let data = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=${api}&contentType=json`);
+    console.log(await data.json());
+}
