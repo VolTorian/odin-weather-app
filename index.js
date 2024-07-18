665134dc52871080aa7b98b888b77c343770aa7f
VolTorian <33360939+VolTorian@users.noreply.github.com>
2024-07-18 01:57:43 -0700

    enter a city using a form

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const api = "CRPB2EX2LBDSGYUZNHK3LM27N";
 
-console.log(document.querySelector("h1").textContent);
+const cityInput = document.getElementById("city");
+const form = document.querySelector("form");
+form.onsubmit = getWeather;
 
 async function getWeather() {
-    let city = prompt("Enter the name of a city:");
+    let city = cityInput.value;
     city = city.replace(/ /g, "%20");
 
-    let data = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=${api}&contentType=json`);
-    console.log(await data.json());
+    let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=${api}&contentType=json`);
+    let data = await response.json();
+    console.log(data.currentConditions.temp);
+    console.log(data.description);
 }
